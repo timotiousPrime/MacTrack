b0fd3be92f00ff672a066605357a5b41b311a337
FiltecAutomation <tim@filtec.co.za>
2023-07-11 17:22:48 +0200

    add user profile url and add to admin page

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User_Profile
+from django.contrib.auth.models import User
+
+# Unregister initial users
+admin.site.unregister(User)
 
 # Register your models here.
+admin.site.register(User, User_Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = User_Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+    inlines = [ProfileInline]
--- members/urls.py
+++ members/urls.py
@@ -5,4 +5,5 @@ from members import views
 urlpatterns = [
     path("login/", views.login_view, name="Login"),
     path("logout/", views.logout_view, name="Logout"),
+    path("<str:username>/", views.userProfile, name="User_Profile"),
 ]
