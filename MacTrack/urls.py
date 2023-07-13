56a487e860ac0c4ef3e42ddffb0fb1373eaf0152
timotiousPrime <timodb031@gmail.com>
2023-07-13 22:18:04 +0200

    add authentication layer

--- MacTrack/urls.py
+++ MacTrack/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('users/', include('django.contrib.auth.urls')),
     path('users/', include('members.urls')),
 ]
