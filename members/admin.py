0370ae04df0034122e662f0a412c316c75b97629
timotiousPrime <timodb031@gmail.com>
2023-07-13 18:04:14 +0200

    general updates

--- members/admin.py
+++ members/admin.py
@@ -2,19 +2,23 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from .models import User_Profile
 
+
 admin.site.unregister(Group)
+
 
 class ProfileInline(admin.StackedInline):
     model = User_Profile
 
+
 class UserAdmin(admin.ModelAdmin):
     model = User
-    fields = ['username']
+    fields = ['username', 'password']
     inlines = [ProfileInline]
 
 
-# Unregister initial users
+# # Unregister initial users
 admin.site.unregister(User)
+
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
