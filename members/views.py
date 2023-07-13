71119476b2586868b5e485cae1b273639e78b9ae
timotiousPrime <timodb031@gmail.com>
2023-07-13 18:03:39 +0200

    update views so no error occurs

--- members/views.py
+++ members/views.py
@@ -1,47 +1,68 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
-from members.forms import Login_Form, User_Profile
-
+from members.forms import Login_Form, User_Profile_Form
+from django.contrib.auth.models import User
 from members.models import User_Profile
 
 # Create your views here.
 
-def login_view(request):
+# LoginView
+def loginView(request):
+    if request.method == "GET":
+        context = {
+            'form': Login_Form()
+        }
+        return render(request, 'members/LoginPage.html', context)
+    
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        print('method is post')
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is None:
+            print('user is none')
             context = {
                 'error': "Username and/or password is incorrect",
                 "form": Login_Form
             }
-            return render (request, 'members/loginPage.html', context)
+            return render(request, 'members/loginPage.html', context)
         else:
             login(request, user)
-            print ("User has been authenticated!")
-            print (user)
+            print('login successful')
             return redirect('Home')
-    else:
-        context = {
-            'form': Login_Form
-        }
-        return render(request, 'members/loginPage.html', context)
-    
 
-def logout_view(request):
+
+# LogoutView
+def logoutView(request):
     logout(request)
     return redirect('Home')
 
 
-def userProfile(request, username):
+def userProfileView(request):
+    # userProfileView
+    if request.method == 'GET':
+        try:
+            profile = request.user.user_profile
+        except User_Profile.DoesNotExist:
+            context = {
+                'errror': "There was an error retrieving this user's profile. Please contect your administrator.",
+            }
+            return redirect('Home')
+    
+        context = {
+            'profile': profile
+        }
+        return render(request, 'members/userProfile.html', context)
 
-    profiles = User_Profile.objects.all()
-    print("*********************************************************")
-    # print (profiles)
-    context = {
-        # "user": User_Profile.objects.get(username=username),
-        "user": "User-TEST",
-        "userProfileForm": User_Profile
-    }
-    return render (request, "members/userProfile.html", context)
+    # updateUserProfileView
+    if request.method == 'POST':
+        user = request.user
+        form = User_Profile_Form(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('getUserProfile')
+
+    context = {}
+    return render(request, 'members/updateUserProfile.html', context)
+
+
