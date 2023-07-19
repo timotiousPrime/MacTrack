1b5f32fa9ebffda77a4ef1c322be14fffb28d538
timotiousPrime <timodb031@gmail.com>
2023-07-19 21:42:58 +0200

    create TaskTime model

---
+++ timesheets/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+# Get user
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class TaskTime(models.Model):
+    JOB_DESCRIPTIONS = [
+        ("Adm", "Admin"),
+        ("Des", "Design"),
+        ("Doc", "Documentation"),
+        ("Dwg", "Drawings"),
+        ("Gen", "General"),
+    ]
+
+    jobCode = models.CharField(max_length=16, default="General", null=False, blank=False)
+    description = models.CharField(max_length=3, default="Gen", choices=JOB_DESCRIPTIONS, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    time_spent=models.TimeField()
+
+    def __str__(self) -> str:
+        return self.jobCode + ': ' + str(self.times_spent)
