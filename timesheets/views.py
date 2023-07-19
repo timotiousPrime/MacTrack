191aee14d8622ea8ef072cbc33f0e6d12b258db7
timotiousPrime <timodb031@gmail.com>
2023-07-19 23:12:04 +0200

    creat view for task timer

--- timesheets/views.py
+++ timesheets/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.contrib.auth.models import User
+from .models import TaskTime
 
 # Create your views here.
+def task_timer(request):
+    
+    user = get_object_or_404(User, username=request.user.username)
+    # user_task_times = get_list_or_404(TaskTime, user=request.user.id)
+
+    context = {
+        'user': user,
+        # 'userTaskTimes': user_task_times,
+    }
+    return render(request, 'timesheets/taskTimer.html', context)
