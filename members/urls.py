c7210de2b68fab92b650dd9b0352ac247cb43243
timotiousPrime <timodb031@gmail.com>
2023-07-20 11:16:57 +0200

    update position of user profile path

--- members/urls.py
+++ members/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from members import views
 
 urlpatterns = [
-    path("<str:username>/", views.user_profile, name="User_Profile"),
     path("login/", views.login_view, name="Login"),
     path("logout/", views.logout_view, name="Logout"),
+    path("<str:username>/", views.user_profile, name="User_Profile"),
 ]
