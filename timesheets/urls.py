f1465dc3735fc782463e23cfefe098a046455da8
timotiousPrime <timodb031@gmail.com>
2023-07-23 21:58:39 +0200

    add add url for tasktime

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -4,6 +4,7 @@ from timesheets import views
 
 urlpatterns = [
     path("", views.task_timer, name="Task_Timer"),
+    path("add/", views.create_task, name="Create_Timer"),
     path("start/", views.start_timer, name="Start_Timer"),
     path("stop/", views.start_timer, name="Stop_Timer"),
 ]
