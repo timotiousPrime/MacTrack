24d59502dbf22d543aa9f6aebf5b2efba536267a
timotiousPrime <timodb031@gmail.com>
2023-07-23 21:58:16 +0200

    update tasktimer form elapsedTime widget

--- timesheets/forms.py
+++ timesheets/forms.py
@@ -1,25 +1,33 @@
 from django import forms
 from timesheets.models import TaskTime
+from django.utils import timezone
+
 
 class Task_Timer_Form(forms.ModelForm):
     class Meta:
         model = TaskTime
-        fields = ('job_code', 'description', 'elapsed_time')
+        fields = ("job_code", "description", "elapsed_time")
 
         widgets = {
-            'job_code': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder':"Job Code",
-                }),
-            'description': forms.Select(attrs={'class': 'form-select'}),
-            'elapsed_time': forms.TimeInput(attrs={
-                'class': 'form-control',
-                'type': "time",
-                'id': 'inputTime',
-                'name': 'time',
-                'value': "00:00:00"
-                }),
+            "job_code": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Job Code",
+                }
+            ),
+            "description": forms.Select(attrs={"class": "form-select"}),
+            "elapsed_time": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "pattern": "\d{2}:\d{2}",
+                    "title": "Enter duration in the format: HH:MM",
+                    "id": "elapsed_time_input",
+                    "name": "time",
+                    "value": "00:00",
+                }
+            ),
         }
+
 
 # class User_Profile(forms.ModelForm):
 #     class Meta:
