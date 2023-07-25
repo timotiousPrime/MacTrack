3eacdd5e419841807021a7cc195513b4abefe829
timotiousPrime <timodb031@gmail.com>
2023-07-25 16:47:07 +0200

    remove commented-out user-profile

--- timesheets/forms.py
+++ timesheets/forms.py
@@ -27,16 +27,3 @@ class Task_Timer_Form(forms.ModelForm):
                 }
             ),
         }
-
-
-# class User_Profile(forms.ModelForm):
-#     class Meta:
-#         model = User_Profile
-#         fields = ('firstName', 'lastName', 'role', 'is_active')
-
-#         # widgets = {
-#         #     'firstName': forms.TextInput(attrs={'class': 'form-control'}),
-#         #     'lastName': forms.TextInput(attrs={'class': 'form-control'}),
-#         #     'role': forms.Select(attrs={'class': 'form-control'}),
-#         #     'is_active': forms.BooleanField(attrs={'class': 'form-control'}),
-#         # }
