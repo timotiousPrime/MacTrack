fad71f9b0f24194cb7f63f3c7f83894bb6a86746
timotiousPrime <timodb031@gmail.com>
2023-08-02 17:00:51 +0200

    add generic view for projects landing page

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def projects_landing_page(request):
+    context = {
+        "greeting": "Welcome to the projects page!"
+    }
+    return render(request, "projects/projectsPage.html", context)
