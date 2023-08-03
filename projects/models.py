32d697f807c92d9b2603d29c378e50fcbe9cbffe
timotiousPrime <timodb031@gmail.com>
2023-08-03 12:56:11 +0200

    update Customer and Project Models

--- projects/models.py
+++ projects/models.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length=40, blank=False, null=False)
-    code = models.CharField(max_length=8)
-    phone_number = models.CharField(max_length=13)
-    fax_number = models.CharField(max_length=13)
-    email = models.EmailField()
-    contact_person = models.CharField(max_length=40)
+    code = models.CharField(max_length=8, blank=False, null=False, unique=True)
+    phone_number = models.CharField(max_length=13, blank=True, null=True)
+    fax_number = models.CharField(max_length=13, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    contact_person = models.CharField(max_length=40, blank=True, null=True)
 
-
+    def __str__(self):
+        return self.name
 
 class Project(models.Model):
     PROJECT_STATUS = [
@@ -24,10 +25,14 @@ class Project(models.Model):
     ]
     customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
     description = models.CharField(max_length=144)
-    isHighPriority = models.BooleanField(default=False)
+    is_high_priority = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     job_code = models.CharField(max_length=8, primary_key=True, blank=False, null=False)
     status = models.CharField(max_length=4, default="Des", choices=PROJECT_STATUS, null=False, blank=False)
     created_by = models.ForeignKey(User, on_delete=models.RESTRICT)
     project_captains = models.ManyToManyField(User, related_name="projects")
     purchase_order_number = models.CharField(max_length=24)
+
+    def __str__(self) -> str:
+        return self.job_code
 
