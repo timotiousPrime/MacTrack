0472207ed4f1d6e3444125d2d2ac9932d19c7744
timotiousPrime <timodb031@gmail.com>
2023-08-03 17:25:58 +0200

    update projects view(WIP), add create view

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
+from projects.forms import ProjectForm
+from projects.models import Project
 # Create your views here.
 
 def projects_landing_page(request):
+    projects = Project.objects.all()
     context = {
-        "greeting": "Welcome to the projects page!"
+        "projects_form": ProjectForm,
+        "projects": projects
     }
     return render(request, "projects/projectsPage.html", context)
+
+def create_Project(request):
+        
+    context = {
+        "projects_form": ProjectForm
+    }
+    return render(request, 'projects/addProject.html', context)
