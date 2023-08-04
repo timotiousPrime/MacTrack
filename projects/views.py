2ec4c98555c3e5a14b9ebbff8a2adf01b0d54311
timotiousPrime <timodb031@gmail.com>
2023-08-04 08:15:51 +0200

    add view to add new projects

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from projects.forms import ProjectForm
 from projects.models import Project
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def projects_landing_page(request):
@@ -12,6 +14,14 @@ def projects_landing_page(request):
     return render(request, "projects/projectsPage.html", context)
 
 def create_Project(request):
+    if request.method == "POST":
+        user = get_object_or_404(User, username=request.user.username)
+        form = ProjectForm(request.POST)
+        if form.is_valid:
+            project = form.save(commit=False)
+            project.created_by = user
+            project.save()
+            return redirect("Projects_Page")
         
     context = {
         "projects_form": ProjectForm
