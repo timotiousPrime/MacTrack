dc409481e564c3e90a5b12bfe80359270ad35915
Tim <timodb031@gmail.com>
2023-08-23 13:15:26 +0200

    set debug false

--- MacTrack/settings.py
+++ MacTrack/settings.py
@@ -30,10 +30,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['evening-shore-46569-0cd8556ecf85.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['evening-shore-46569-0cd8556ecf85.herokuapp.com', 
+                 '127.0.0.1']
 
 
 # Application definition
