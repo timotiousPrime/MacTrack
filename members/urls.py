57d7d980731d56cc248a3b5d6c643b08d9f5fa64
Tim <timodb031@gmail.com>
2023-09-08 12:00:43 +0200

    add user task history page url route

--- members/urls.py
+++ members/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", views.login_view, name="Login"),
     path("logout/", views.logout_view, name="Logout"),
     path("<str:username>/", views.user_profile, name="User_Profile"),
+    path("<str:username>/taskHistory", views.user_task_history, name="User_Task_History"),
 ]
