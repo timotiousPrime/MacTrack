94fabcdaefd45a7f5cef4bb39a4766967547473a
Tim <timodb031@gmail.com>
2023-09-15 12:07:31 +0200

    Chart updates

--- members/views.py
+++ members/views.py
@@ -49,17 +49,24 @@ def user_profile(request, username):
 
     for t in user_task_times:
         job_code = str(t.job_code)
-        elapsed_time = str(t.elapsed_time) if t.elapsed_time is not None else datetime.timedelta(seconds=0)
-        # print(t)
+        if t.elapsed_time is None:
+            elapsed_time = datetime.timedelta(seconds=0)
+        else:
+            elapsed_time = str(t.elapsed_time)
+        # elapsed_time = str(t.elapsed_time) if t.elapsed_time is not None else datetime.timedelta(seconds=0)
+        print(elapsed_time)
         if t.job_code not in job_code_hours:
             job_code_hours[job_code] = elapsed_time
         else:
             job_code_hours[job_code] += elapsed_time
-        # print(job_code_hours)
+    
+    print("Job Code Hours: ", job_code_hours)
 
     # We need lists for the input for the x and y axis's respectively
     jc_list = list(job_code_hours.keys())
+    print("Job Codes: ", jc_list)
     et_list = list(job_code_hours.values())
+    print("Elapsed Times: ",et_list)
 
     # We want the time in hours
     if et_list is not None:
@@ -73,7 +80,6 @@ def user_profile(request, username):
     user = get_object_or_404(User, username=username)
     # user_tasks = get_list_or_404(TaskTime, user)
     user_tasks = list(TaskTime.objects.filter(user=user).exclude(elapsed_time=None).order_by('-id'))[:5]
-    print(user_tasks)
 
     context = {
         'user': user,
--- reports/views.py
+++ reports/views.py
@@ -75,3 +75,38 @@ def task_timer_reports(request):
 
     return render(request, "reports/taskTimerPage.html", context)
 
+def dashboard_project_times_chart(request):
+    # Get all user task times
+    task_times = list(TaskTime.objects.filter(user=request.user.id))
+
+    # Add up all the time for each job code
+    job_code_hours = {}
+
+    for t in task_times:
+        if t.job_code not in job_code_hours:
+            job_code_hours[str(t.job_code)] = t.elapsed_time
+        else:
+            job_code_hours[str(t.job_code)] += t.elapsed_time
+
+    # We need lists for the input for the x and y axis's respectively
+    jc_list = list(job_code_hours.keys())
+    et_list = list(job_code_hours.values())
+
+
+    # We want the time in hours
+    if et_list is not None:
+        et_list = [t.total_seconds() / 3600 for t in et_list]
+    else:
+        et_list = []
+
+
+    script, div = get_graph_components(jc_list, et_list)
+
+    context = {
+        "reports": "Here are some task timer reports",
+        "script": script,
+        "div": div
+    }
+
+    return render(request, "reports/taskTimerPage.html", context)
+
