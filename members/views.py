47c109449e652b05284760074041f714b415fcb9
Tim <timodb031@gmail.com>
2023-09-18 15:12:11 +0200

    fixed user Task Times chart in dashboard

--- members/views.py
+++ members/views.py
@@ -40,49 +40,31 @@ def logout_view(request):
     return redirect('Home')
 
 def user_profile(request, username):
-
     # Get all task times 
     user_task_times = TaskTime.objects.filter(user=request.user.id).exclude(elapsed_time=None)
 
     # Add up all the time for each job code
     job_code_hours = {}
 
-    # for t in user_task_times:
-    #     job_code = str(t.job_code)
-    #     if t.elapsed_time is None:
-    #         elapsed_time = datetime.timedelta(seconds=0)
-    #     else:
-    #         elapsed_time = t.elapsed_time
-    #     # elapsed_time = str(t.elapsed_time) if t.elapsed_time is not None else datetime.timedelta(seconds=0)
-    #     print(elapsed_time)
-    #     if t.job_code not in job_code_hours:
-    #         job_code_hours[job_code] = elapsed_time.seconds
-    #     else:
-    #         job_code_hours[job_code] += elapsed_time.seconds
+    # Job Codes
+
+    # Task Time
 
     for t in user_task_times:
         if t.job_code not in job_code_hours:
             job_code_hours[t.job_code] = (t.elapsed_time.seconds / 3600)
         else:
             job_code_hours[t.job_code] += (t.elapsed_time.seconds  / 3600)
+            print("TYPE:", type(job_code_hours[t.job_code]))
+
+    # We need lists for the input for the x and y axis's respectively
+    jc_list = [project.job_code for project in job_code_hours.keys()]
+    print("Job Codes: ", jc_list)
+    et_list = list(job_code_hours.values())
+    print("Elapsed Times: ",et_list)
     
-    print("Job Code Hours: ", job_code_hours)
 
-    # # We need lists for the input for the x and y axis's respectively
-    # jc_list = list(job_code_hours.keys())
-    # print("Job Codes: ", jc_list)
-    # et_list = list(job_code_hours.values())
-    # print("Elapsed Times: ",et_list)
 
-    jc_list, et_list = list(job_code_hours.keys()), list(job_code_hours.values())
-
-    print(jc_list, et_list)
-
-    # We want the time in hours
-    # if et_list is not None:
-    #     et_list = [t.total_seconds() / 3600 for t in et_list if t is not None and hasattr(t, 'total_seconds')]
-    # else:
-    #     et_list = []
 
     script, div = get_graph_components(jc_list, et_list)
 
