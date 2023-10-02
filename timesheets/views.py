3e9493eac9c7ebbfe92a9d3f7967f4ba46e50f59
Tim <timodb031@gmail.com>
2023-10-02 11:29:22 +0200

    decouple queries from task timer view - move to ut

--- timesheets/views.py
+++ timesheets/views.py
@@ -8,92 +8,20 @@ from django.contrib.auth.models import User
 from timesheets.forms import Task_Timer_Form
 # Time functions
 from django.utils import timezone
-from datetime import timedelta
 # Utility Functions
-from .utilities import calculate_total_time, stop_running_tasks
+from .utilities import get_task_timer_context, stop_running_tasks, update_ongoing_tasks
 
 
 # Create your views here.
 @login_required
 def task_timer(request):
-    today = timezone.localdate()
-    user = get_object_or_404(User, username=request.user.username)
-    user_tasks = TaskTime.objects.filter(user=request.user.id)
-    todays_tasks = user_tasks.filter(date_created__date=today)
+    userId = get_object_or_404(User, username=request.user.username).id
 
-    """
-    Check for ongoing tasks from previous days, and create new tasks for today if any ongoing
-    """
-    # Get ongoing tasks from user
-    users_ongoing_tasks = user_tasks.filter(is_ongoing=True).exclude(date_created=today)
+    # Update ongoing tasks
+    update_ongoing_tasks(userId)
 
-    # Create a set of ongoing tasks - no duplicate tasks with the same jobcodes, ancillarycodes and descriptions
-    og_tasks = set()
-    for t in users_ongoing_tasks:
-        og = (t.job_code, t.ancillary_code, t.description)
-        og_tasks.add(og)
-    
-    # Check if any of the ongoing tasks are in todays tasks already, if not create new ongoing tasks for today
-    for og in og_tasks:
-        task_exists = False
-        for tt in todays_tasks:
-            if og[0] == tt.job_code and og[1] == tt.ancillary_code and og[2] == tt.description:
-                task_exists = True
-        if not task_exists:
-            newTask = TaskTime(user=request.user, ancillary_code=og[1], description=og[2], job_code=og[0], is_ongoing=True, is_running=False, elapsed_time=timedelta(0))
-            newTask.save()
-
-
-    todays_tasks = todays_tasks.exclude(elapsed_time=None)
-    # Calculate the total time
-    todays_total_hrs, todays_total_minutes = calculate_total_time(todays_tasks)
-
-    # Get Longest Task
-    longest_task = None
-    # Get total Design Time
-    total_design_time = timedelta()
-
-    for task in todays_tasks:
-        if longest_task is None or task.elapsed_time > longest_task.elapsed_time:
-            longest_task = task
-        if task.description == "Des":
-            total_design_time += task.elapsed_time
-
-    # Calculate total hours and minutes
-    total_design_hours, remainder = divmod(total_design_time.seconds, 3600)
-    total_design_minutes = remainder // 60
-
-    # Get todays task count, exclude breaks
-    todays_task_count = todays_tasks.exclude(ancillary_code="Break")
-
-    # Get Running Task
-    try:
-        running_task = user_tasks.get(is_running=True)
-    except TaskTime.DoesNotExist:
-        # Handle the case where there is no running task
-        running_task = None  # You can set it to None or any other default value
-
-    print("Running Task: ", running_task)
-
-    if running_task == None:
-        running_task_info = None
-    else:
-        running_task_info = {"id": running_task.id, "time_started": running_task.time_started, "elapsed_time": running_task.elapsed_time.seconds}
-
-    context = {
-        "title": "Task Timer",
-        "user": user,
-        "taskTimerForm": Task_Timer_Form,
-        "userTasks": user_tasks,
-        "todays_tasks": todays_tasks,
-        "todays_total_hrs" : todays_total_hrs,
-        "todays_total_minutes" : todays_total_minutes,
-        "longest_task" : longest_task,
-        "total_design_hours" : total_design_hours,
-        "total_design_minutes" : total_design_minutes,
-        "todays_task_count": todays_task_count,
-        "running_task": running_task_info,
-    }
+    # Context: title, task_timer_form, todays_tasks, todays_stats
+    context = get_task_timer_context(userId)
 
     return render(request, "timesheets/taskTimerPage.html", context)
 
