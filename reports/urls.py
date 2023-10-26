7c60d4ae725ff77ea8d884ad35185f1edcd8b0d3
Tim <timodb031@gmail.com>
2023-10-26 14:54:11 +0200

    add admin reports url path

--- reports/urls.py
+++ reports/urls.py
@@ -5,4 +5,5 @@ from reports import views
 urlpatterns = [
     path("", views.reports_landing_page, name="Reports_Page"),
     path("taskTimers/", views.task_timer_reports, name='Task_Timer_Reports'),
+    path("admin/", views.admin_reports_page, name='Admin_Reports'),
 ]
