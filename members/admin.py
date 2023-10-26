9099cd4e203c6a9c5f81a5c1a82fdf88259cfbcb
Tim <timodb031@gmail.com>
2023-10-26 14:49:13 +0200

    extend user model to User_Profile, add to admin

--- members/admin.py
+++ members/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from timesheets.models import TaskTime
-
+from django.contrib.auth.models import User
+from .models import User_Profile
 
 # Register your models here.
 
 admin.site.register(TaskTime)
+
+# class ProfileInline(admin.StackedInline):
+#     model = User_Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username", "email"]
+    # inlines = [ProfileInline]
+
+
+# Unregister initial User so we can update the modified User for admin
+admin.site.unregister(User)
+
+# Reregister User and profile
+admin.site.register(User, UserAdmin)
+admin.site.register(User_Profile)
