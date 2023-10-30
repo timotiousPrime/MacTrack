7814a2df7baa2faeeff7f6f40831ce251ca3f158
Tim <timodb031@gmail.com>
2023-10-30 08:09:14 +0200

    add url for taskTimes full report

--- reports/urls.py
+++ reports/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.reports_landing_page, name="Reports_Page"),
     path("taskTimers/", views.task_timer_reports, name='Task_Timer_Reports'),
     path("admin/", views.admin_reports_page, name='Admin_Reports'),
+    path("admin/taskTimes", views.full_task_time_report, name='Task_Time_Report'),
 ]
