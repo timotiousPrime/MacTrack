a3bc1890a905ddb72b3751bdc3ee640bfe6ca127
Tim <timodb031@gmail.com>
2023-11-06 13:37:46 +0200

    add urls to edit and delete task history

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("stop/<int:task_id>", views.stop_timer, name='Stop_Timer'),
     path("edit/<int:task_id>", views.edit_timer, name='Edit_Timer'),
     path("delete/<int:task_id>", views.delete_timer, name='Delete_Timer'),
+    path("delete/history/<int:task_id>", views.delete_history_timer, name='Delete_History_Timer'),
+    path("<str:task_id>/editTaskHistory", views.edit_timer_task_history, name="Edit_User_Task_History"),
 ]
