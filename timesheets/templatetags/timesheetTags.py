4859cc8f71d69ed015e332201154164253f91286
Tim <timodb031@gmail.com>
2023-11-06 14:04:46 +0200

    add debugging prints for tag

--- timesheets/templatetags/timesheetTags.py
+++ timesheets/templatetags/timesheetTags.py
@@ -1,13 +1,13 @@
 from datetime import timedelta, datetime
 from django.utils import timezone
 from django import template
-from django.template.defaultfilters import pluralize
 
 register = template.Library()
 
 @register.filter
-def isEditable(value, mode=""):
+def isEditable(value):
     now = timezone.now()
     aWeekAgo = now-timedelta(days=7)
+    print(value, type(value), type(aWeekAgo))
     return value > aWeekAgo
 
