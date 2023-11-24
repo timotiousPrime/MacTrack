c39f6212cd4c703bbd114f6770e2f78215208b0d
Tim <timodb031@gmail.com>
2023-11-24 10:54:15 +0200

    add path for customer and add_customer

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,6 @@ from projects import views
 urlpatterns = [
     path("", views.projects_landing_page, name="Projects_Page"),
     path("add/", views.create_Project, name="Create_Project"),
+    path("customers/", views.customers_page, name="Customers_Page"),
+    path("add_customer/", views.add_customer, name="Add_Customer"),
 ]
