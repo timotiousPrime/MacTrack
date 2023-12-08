dcdf396c6f4d1fe5fc0b391d1b75538af9b9121d
Tim <timodb031@gmail.com>
2023-12-08 10:48:25 +0200

    removed unused variable

--- main/static/main/js/selectTableRow.js
+++ main/static/main/js/selectTableRow.js
@@ -1,17 +1,12 @@
 $('.clickableRow').on("click", (e) => {
     const tr = e.currentTarget
-    const trId = tr.id.split("-")[1]
+    // const trId = tr.id.split("-")[1]
 
     let isSelected = $(tr).hasClass( "selectedRow" )
     
     $(".selectedRow").removeClass("selectedRow bg-info")
 
     if (!isSelected) {
-        console.log(tr.id + " is NOT selected!")
         $(tr).addClass( "selectedRow bg-info" )
     }
-
-
-
-    console.log(trId)
 })
