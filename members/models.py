904d8fa8fa67a58d2421955ee50544e49e28efc1
Tim <timodb031@gmail.com>
2024-03-28 09:07:24 +0200

    add related name to user prop in userProfile model

--- members/models.py
+++ members/models.py
@@ -14,7 +14,7 @@ class User_Profile(models.Model):
         ("Wel", "Welder"),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
     firstName = models.CharField(max_length=48, null=True, blank=True)
     lastName = models.CharField(max_length=48, null=True, blank=True)
     role = models.CharField(max_length=3, choices=USER_ROLE, default="Gen")
