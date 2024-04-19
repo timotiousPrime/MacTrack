0f951c1f35b91621dbbe0cd7903420fee96e1297
timotiousPrime <timodb031@gmail.com>
2024-04-19 07:38:51 +0200

    add urls for add and edit users

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import UserListView
 from members import views
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path("logout/", views.logout_view, name="Logout"),
     path("<str:username>/", views.user_profile, name="User_Profile"),
     path("<str:username>/taskHistory", views.user_task_history, name="User_Task_History"),
+    path("list", UserListView.as_view(), name="User_List"),
+    path("add_user", views.addNewUser, name="Add_User"),
+    path("edit_user/<str:userId>", views.editUser, name="Edit_User"),
 ]
